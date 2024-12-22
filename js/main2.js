c8acd77e01f797c9bcb8f2758d363bd122fff076
Maim24 <maimm2024@gmail.com>
2024-12-22 18:10:11 +0200

    Add files via upload

---
+++ js/main2.js
@@ -0,0 +1,15 @@
+var TopBtn = document.getElementById("topBtn");
+ window.onscroll = function(){
+    if(document.documentElement.scrollTop > 300 ){
+        TopBtn.style.display="block" ;
+    }
+    else{
+        TopBtn.style.display = "none";
+    }
+ };
+TopBtn.onclick = function(){
+    window.scroll ({
+        top:0,
+        behavior:"smooth"
+    })
+}
